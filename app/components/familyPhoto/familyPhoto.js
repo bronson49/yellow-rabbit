3d76511e96bfcb302825e8681c4b9b957719558d
bronson49 <bcharly49@gmail.com>
2019-04-17 18:04:10 +0300

    tt

---
+++ app/components/familyPhoto/familyPhoto.js
@@ -0,0 +1,18 @@
+import jqueryui from "jquery-ui";
+
+const familyPhotoFunc = function () {
+    $('#family-slide').slider({
+        value : 0,
+        min : 0,
+        max : 100,
+        step : 1,
+        create: function( event, ui ) {
+            val = $( "#slider" ).slider("value");
+        },
+        slide: function( event, ui ) {
+            console.log(val);
+        }
+    });
+};
+
+export {familyPhotoFunc}
--- app/core/main.js
+++ app/core/main.js
@@ -1,6 +1,8 @@
 import {borderWrapperFunc} from "../components/borderWrapper/borderWrapper";
+import {familyPhotoFunc} from "../components/familyPhoto/familyPhoto";
 
 $(document).ready(function () {
     borderWrapperFunc();
+  //  familyPhotoFunc();
 });
 
