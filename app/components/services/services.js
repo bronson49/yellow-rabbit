7c1f01f8d16c3ac8560a0ab2016241d5ddf07ed7
bronson49 <bcharly49@gmail.com>
2019-04-18 15:04:06 +0300

    services complete

--- app/components/services/services.js
+++ app/components/services/services.js
@@ -1,3 +1,4 @@
+//import tabs from 'webpack-jquery-ui/tabs';
 
 const servicesFunc = function () {
     $('.services-list-item').click(function () {
@@ -5,6 +6,21 @@ const servicesFunc = function () {
            $(this).removeClass('services-item-active') ;
         });
         $(this).addClass('services-item-active');
+    });
+
+    $('.services-list-item').each(function (index, value) {
+        $(this).attr('data-tab-index', index);
+    });
+    $('.services-img-item').each(function (index, value) {
+        $(this).attr('data-tab-index', index);
+    });
+
+    $('.services-list-item').click(function () {
+        let href = $(this).attr('data-tab-index');
+        $('.services-img-item').each(function () {
+            $(this).removeClass('active-tab');
+        });
+        $('[data-tab-index="'+href+'"]').addClass('active-tab');
     });
 };
 
