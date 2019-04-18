5725d74c9aa0f97abd7ad33883ae14bc3680081e
bronson49 <bcharly49@gmail.com>
2019-04-18 14:11:36 +0300

    family ui slide

--- app/components/familyPhoto/familyPhoto.js
+++ app/components/familyPhoto/familyPhoto.js
@@ -1,19 +1,38 @@
 import slider from 'webpack-jquery-ui/slider';
 
 const familyPhotoFunc = function () {
-    let val;
+
+    let photoList = $('.family-photo-list');
+    let photoListWidth = $(photoList).width();
+    let maxSlide = photoListWidth - window.innerWidth + 190;
+    let stepClick = Math.floor(maxSlide/5);
+
     $('#family-slide').slider({
         value : 0,
         min : 0,
-        max : 100,
+        max : maxSlide,
         step : 1,
         create: function( event, ui ) {
-           // val = $( "#slider" ).slider("value");
+
         },
         slide: function( event, ui ) {
-           // console.log(val);
+           // console.log(ui.value);
+            $(photoList).css({'transform':'translateX(-'+ui.value+'px)'});
         }
     });
+
+    $('.family-slide-next').click(function () {
+        let val = $('#family-slide').slider('value');
+        if (val>=maxSlide-50) return;
+        $('#family-slide').slider('value', val+stepClick);
+        $(photoList).css({'transform':'translateX(-'+(val+stepClick)+'px)'});
+    });
+    $('.family-slide-prev').click(function () {
+        let val = $('#family-slide').slider('value');
+        $('#family-slide').slider('value', val-stepClick);
+        $(photoList).css({'transform':'translateX(-'+(val-stepClick)+'px)'});
+    });
+
 };
 
 export {familyPhotoFunc}
