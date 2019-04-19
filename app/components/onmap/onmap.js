09954fc7ec21d7a6e3af44295bce1c1af76e5091
bronson49 <bcharly49@gmail.com>
2019-04-19 15:09:04 +0300

    onmap complete

--- app/components/onmap/onmap.js
+++ app/components/onmap/onmap.js
@@ -2,7 +2,9 @@ import slickCarousel from 'slick-carousel/slick/slick.min';
 
 const onmapFunc = function () {
     $('.onmap-slider-body').slick({
-
+        prevArrow: $('.onmap-prev'),
+        nextArrow: $('.onmap-next'),
+        dots: true ,
     });
 };
 
