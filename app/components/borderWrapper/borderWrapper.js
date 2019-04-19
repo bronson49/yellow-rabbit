7b55fb4b93ee2ccb68b964e6576fea65f3da274d
bronson49 <bcharly49@gmail.com>
2019-04-19 17:02:10 +0300

    block scroll complete

--- app/components/borderWrapper/borderWrapper.js
+++ app/components/borderWrapper/borderWrapper.js
@@ -24,6 +24,80 @@ const borderWrapperFunc = function () {
 
         $('main').css({'opacity':'1'});
     }
+
+
+    var delta = 0,
+        sections = $('.container'),
+        sectionsOffset = [],  // массив для отступов всех блоков
+        sectionsLength= 0, // кол-во блоков
+        canScroll = true;
+
+    $(sections).each(function () { // получить отступы всех блоков
+        sectionsOffset.push($(this).offset().top);
+    });
+    sectionsLength = sectionsOffset.length; // получить кол-во блоков
+
+
+    $('.scrollUp').click(function () {
+        let yOffset = window.pageYOffset;
+        wheelUp(yOffset);
+    });
+    $('.scrollDown').click(function () {
+        let yOffset = window.pageYOffset;
+        wheelDown(yOffset);
+    });
+    
+    $('.border-page-list li').click(function () {
+        let i = $(this).index();
+        changePage(i);
+    });
+
+    function changePage(i) {
+        clearPageList();
+        $('html, body').stop().animate({ scrollTop : sectionsOffset[i]-90 }, 800);
+        $('.border-page-list li')[i].classList.add('page-active');
+    }
+    function clearPageList() {
+        $('.page-active').each(function () {
+            $(this).removeClass('page-active')
+        });
+    }
+
+    document.body.addEventListener('wheel', handleScroll, { passive: false } );  // отменить пассивность ивента
+    function handleScroll(e) {
+        e.preventDefault();
+        if  (!canScroll) return ;
+        delta = e.deltaY ;   // получить направление прокрутки
+        canScroll = false ;
+        let yOffset = window.pageYOffset;
+        if (delta > 0){
+            wheelDown(yOffset);
+        } else if (delta < 0){
+            wheelUp(yOffset);
+        }
+        setTimeout(function () {
+            canScroll = true;
+        }, 800);
+    }
+    function wheelUp(scrl){
+        for (var i= sectionsLength - 2 ;i > -1 ; i--){
+            if (scrl-100 > sectionsOffset[i]){
+                changePage(i);
+                return
+            }
+        }
+    }
+    function wheelDown(scrl){
+        for (var i= 1 ;i < sectionsLength ; i++ ){
+            if (scrl+100 < sectionsOffset[i] ){
+                changePage(i);
+                return
+            }
+        }
+    }
+
+
+
 };
 
 export {borderWrapperFunc}
