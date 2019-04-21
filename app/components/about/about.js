f7a4902a5a75fc7cb00738d7a9960d0bb7322c89
bronson49 <bcharly49@gmail.com>
2019-04-21 13:49:34 +0300

    mobile about complete

--- app/components/about/about.js
+++ app/components/about/about.js
@@ -7,7 +7,26 @@ const aboutFunc = function () {
             nextArrow: $('.about-next'),
             dots: false ,
         });
+
+        const showBtn = ` <a href="#" class="about-show-text desktop-none">Подробнее</a>`;
+        $('.about-item-text').each(function (i, val) {
+            let thisHeight = $(this).height();
+            let contentHeight = $(this).find('p').height();
+
+            if (contentHeight > thisHeight) {
+                $(this).addClass('show-more');
+                $(this).append(showBtn);
+            }
+        });
+
+        $('.about-show-text').click(function (e) {
+            e.preventDefault();
+            let parentBlock = $(this).parent('.about-item-text');
+            $(parentBlock).removeClass('show-more');
+            $(this).remove();
+        });
     }
+
 };
 
 export {aboutFunc}
