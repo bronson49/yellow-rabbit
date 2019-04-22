83514ef705ab4a95be8d421e29a3a4e58f21bd7a
bronson49 <bcharly49@gmail.com>
2019-04-22 19:15:45 +0300

    design cry

--- app/components/tariffs/tariffs.js
+++ app/components/tariffs/tariffs.js
@@ -1,5 +1,14 @@
 
 const tariffsFunc = function () {
+
+    $('.offers-item-order').click(function () {
+        $(this).closest('.tariffs-offers-item').addClass('offers-form-open');
+    });
+    $('.closeOffer').click(function (e) {
+        e.stopPropagation();
+       $(this).closest('.tariffs-offers-item').removeClass('offers-form-open');
+    });
+
     if (window.matchMedia('(max-width : 450px)').matches){
 
         $('.tariffs-offers').slick({
