ca6fc7b228b9fa2f04fb4ac6a00496230135fc48
bronson49 <bcharly49@gmail.com>
2019-04-23 14:38:43 +0300

    slider length

--- app/components/familyPhoto/familyPhoto.js
+++ app/components/familyPhoto/familyPhoto.js
@@ -2,36 +2,39 @@ import slider from 'webpack-jquery-ui/slider';
 
 const familyPhotoFunc = function () {
 
-    let photoList = $('.family-photo-list');
-    let photoListWidth = $(photoList).width();
-    let maxSlide = photoListWidth - window.innerWidth + 190;
-    let stepClick = Math.floor(maxSlide/5);
+    setTimeout(function () {
+        let photoList = $('.family-photo-list');
+        let photoListWidth = $(photoList).width();
+        let maxSlide = photoListWidth - window.innerWidth + 190;
+        let stepClick = Math.floor(maxSlide/5);
 
-    $('#family-slide').slider({
-        value : 0,
-        min : 0,
-        max : maxSlide,
-        step : 1,
-        create: function( event, ui ) {
+        $('#family-slide').slider({
+            value : 0,
+            min : 0,
+            max : maxSlide,
+            step : 1,
+            create: function( event, ui ) {
 
-        },
-        slide: function( event, ui ) {
-           // console.log(ui.value);
-            $(photoList).css({'transform':'translateX(-'+ui.value+'px)'});
-        }
-    });
+            },
+            slide: function( event, ui ) {
+                // console.log(ui.value);
+                $(photoList).css({'transform':'translateX(-'+ui.value+'px)'});
+            }
+        });
 
-    $('.family-slide-next').click(function () {
-        let val = $('#family-slide').slider('value');
-        if (val>=maxSlide-50) return;
-        $('#family-slide').slider('value', val+stepClick);
-        $(photoList).css({'transform':'translateX(-'+(val+stepClick)+'px)'});
-    });
-    $('.family-slide-prev').click(function () {
-        let val = $('#family-slide').slider('value');
-        $('#family-slide').slider('value', val-stepClick);
-        $(photoList).css({'transform':'translateX(-'+(val-stepClick)+'px)'});
-    });
+        $('.family-slide-next').click(function () {
+            let val = $('#family-slide').slider('value');
+            if (val>=maxSlide-50) return;
+            $('#family-slide').slider('value', val+stepClick);
+            $(photoList).css({'transform':'translateX(-'+(val+stepClick)+'px)'});
+        });
+        $('.family-slide-prev').click(function () {
+            let val = $('#family-slide').slider('value');
+            $('#family-slide').slider('value', val-stepClick);
+            $(photoList).css({'transform':'translateX(-'+(val-stepClick)+'px)'});
+        });
+    }, 1000);
+
 
 };
 
